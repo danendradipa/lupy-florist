d785cd02cf35ae3a95e53175a64fb4c1ff7368b2
danendradipa <danendrach@gmail.com>
2025-01-14 22:42:22 +0700

    feat:  update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import Category from "./components/Category";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import LocationFaq from "./components/LocationFaq";
 import Navbar from "./components/Navbar";
+import Offered from "./components/Offered";
+import Testimonial from "./components/Testimonial";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Offered />
+      <Category />
+      <WhyChooseUs />
+      <Testimonial />
+      <LocationFaq />
+      <Footer />
     </>
   );
 };
