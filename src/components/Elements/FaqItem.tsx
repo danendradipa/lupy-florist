58c7b15958e930815d32ae0be2b90887804472ee
danendradipa <danendrach@gmail.com>
2025-01-14 22:40:04 +0700

    feat: update FaqItem component to use props and improve UI

--- src/components/Elements/FaqItem.tsx
+++ src/components/Elements/FaqItem.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const FaqItem = () => {
-  return (
-    <div>FaqItem</div>
-  )
+interface FAQItemProps {
+  question: string;
+  answer: string;
+  isOpen: boolean;
+  onClick: () => void;
 }
 
-export default FaqItem
+const FaqItem = ({ question, answer, isOpen, onClick }: FAQItemProps) => {
+  return (
+    <div className="mb-4">
+      <button
+        className="w-full max-w-[1012px] text-left bg-primary text-white p-4 rounded-xl flex justify-between items-center"
+        onClick={onClick}
+      >
+        {question}
+        <span>{isOpen ? "▲" : "▼"}</span>
+      </button>
+      <div
+        className={`w-full max-w-[1012px] bg-default overflow-hidden rounded-b-lg shadow-md transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-[500px] opacity-100 p-4" : "max-h-0 opacity-0 p-0"
+        }`}
+      >
+        {answer}
+      </div>
+    </div>
+  );
+};
+
+export default FaqItem;
