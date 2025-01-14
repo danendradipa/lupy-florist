3bcafd0da7c80466b9d0ade60e7a0bbe5a9f773e
danendradipa <danendrach@gmail.com>
2025-01-14 21:50:53 +0700

    feat: update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#286157',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '2rem',
+          xl: '6rem',
+          '2xl': '6rem',
+        },
+      },
+      fontFamily: {
+        head: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
