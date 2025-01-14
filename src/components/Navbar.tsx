cb9be7d241474bd33948a37389235aa90be6b4aa
danendradipa <danendrach@gmail.com>
2025-01-14 22:04:08 +0700

    feat: add interface for navItem

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,32 @@
-import React from 'react'
+interface NavItem {
+  id: number;
+  name: string;
+  link: string;
+}
+
+const navItem: NavItem[] = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "Product",
+    link: "#product",
+  },
+  {
+    id: 3,
+    name: "Services",
+    link: "#services",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    link: "#contact",
+  },
+];
+
 
 const Navbar = () => {
   return (
